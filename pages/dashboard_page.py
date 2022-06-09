775f30b597364f184e12aed7d3849570a00a9597
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-06-09 18:14:06 +0300

    updated

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -4,4 +4,4 @@ from .base_page import BasePage
 class DashboardPage(BasePage):
     def user_see_dashboard_content_text(self):
         dashboard_content_text = self.driver.find_element(*ResourceManagerLocators.RESOURCE_MNG_DASHBOARD_TEXT).text
-        assert dashboard_content_text == "Select the options you want.", f"Select the options you want. is expected, but {dashboard_text} is displayed"
+        assert dashboard_content_text == "Select the options you want.", f"Select the options you want. is expected, but {dashboard_content_text} is displayed"
